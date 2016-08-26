c96afc47c4be04624536759ae8bbd7624a670106
Michael McCune <msm@redhat.com>
2016-08-26 15:45:59 -0400

    add user option
    also add a default for the project, and clean up the help text some.

--- tools/oshinko-deploy.sh
+++ tools/oshinko-deploy.sh
@@ -8,8 +8,6 @@
 #   * openshift-spark
 #   * daikon-pyspark
 # * you have a file named "server-ui-template.yaml" in the current directory
-# * you have reployed an openshift cluster with `oc cluster up` and have the
-#   default `developer` account active
 #
 # Usage:
 # $ oshinko-deploy.sh {route IP} {project name}
@@ -18,7 +16,7 @@
 # project name -- project to deploy oshinko into
 
 
-while getopts :s:w:p:h opt; do
+while getopts :s:w:p:u:h opt; do
     case $opt in
         s)
             SPARK_IMAGE=$OPTARG
@@ -29,15 +27,22 @@ while getopts :s:w:p:h opt; do
         p)
             PROJECT=$OPTARG
             ;;
+        u)
+            OS_USER=$OPTARG
+            ;;
         h)
-            echo "Usage: oshinko-deploy.sh [-w <hostname to use in exposed route to oshinko-web]"
-            echo "Example: oshinko-deploy.sh -w mywebui.10.16.40.70.xip.io"
-            echo "Usage: oshinko-deploy.sh [-s <spark docker image to use for clusters>]"
-            echo "Example: oshinko-deploy.sh -s myregistry.com:5000/sparkimage"
-            echo "Usage: oshinko-deploy.sh [-p <OpenShift project name to install into>]"
-            echo "Example: oshinko-deploy.sh -p myproject"
-            echo "    results in the oshinko web service exposed at mywebui.10.16.40.70.xip.io"
-            echo "If -w is not set, the default route will be used based on routing suffix, etc set at installation"
+            echo "usage: oshinko-deploy.sh [-w HOSTNAME] [-s IMAGE] [-p PROJECT] [-u USER]"
+            echo
+            echo "deploy the oshinko suite into a running OpenShift cluster"
+            echo
+            echo "optional arguments:"
+            echo "  -h            show this help message"
+            echo "  -w HOSTNAME   hostname to use in exposed route to oshinko-web"
+            echo "  -s IMAGE      spark docker image to use for clusters (default: openshift-spark)"
+            echo "  -p PROJECT    OpenShift project name to install oshinko into (default: myproject)"
+            echo "  -u USER       OpenShift user to run commands as (default: developer)"
+            echo
+            echo "  If -w is not set, the default route will be used based on routing suffix, etc set at installation"
             exit
             ;;
         \?)
@@ -47,11 +52,16 @@ while getopts :s:w:p:h opt; do
     esac
 done
 
-if [ -z $PROJECT ]
+if [ -z "$PROJECT" ]
 then
-    echo "project name not supplied, for example:"
-    echo "./oshinko-deploy.sh -p myproject"
-    exit 1
+    echo "project name not set, using default value"
+    PROJECT=myproject
+fi
+
+if [ -z "$OS_USER" ]
+then
+    echo "user not set, using default value"
+    OS_USER=developer
 fi
 
 oc login -u system:admin
@@ -59,7 +69,7 @@ oc project default
 REGISTRY=$(oc get service docker-registry --no-headers=true | awk -F ' ' '{print $2":"$4}' | sed "s,/TCP$,,")
 
 # reset back to the default development account
-oc login -u developer
+oc login -u $OS_USER
 oc project $PROJECT
 
 # Wait for the registry to be fully up
